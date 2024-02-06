8ca3ab4c4c6fa6c9bed29364e5257ac87782f072
Abirami Shri <shriabi.shri@gmail.com>
2024-02-05 19:21:16 -0500

    Header and Footer created

---
+++ frontend/src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <main>
+      {/* Add the main content of your application */}
+      <p>Welcome to Your React App!</p>
+      {/* You can include other components or content here */}
+    </main>
+  );
+};
+
+export default Main;
